78c02d9859c1884e8b3326950767d9b26175095d
hh-pixel <hamza.haidersyed28@gmail.com>
2024-07-23 11:46:23 +0500

    Added a song when the button is clicked on the homepage

--- homepage.js
+++ homepage.js
@@ -1,8 +1,44 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    const button = document.getElementById('myButton');
-    const messageDiv = document.getElementById('message');
+const button = document.getElementById("myButton"); // Assuming your button has id "myButton"
 
-    button.addEventListener('click', () => {
-        messageDiv.textContent = 'Click any 1 of the things above to explore this website';
-    });
+hasmouseclicked = false;
+
+button.addEventListener("click", function () {
+  button.innerHTML = "Never Gonna Give You Up";
+  hasmouseclicked = true;
 });
+
+myaudio = document.getElementById("myaudio");
+
+audioplayed = 0;
+
+button.addEventListener("mouseover", function () {
+  if (hasmouseclicked) {
+
+    myaudio.play();
+    button.style.position = "absolute";
+    button.style.zIndex = 1;
+    
+    const screenWidth = window.innerWidth;
+    const screenHeight = window.innerHeight;
+    const buttonWidth = button.offsetWidth;
+    const buttonHeight = button.offsetHeight;
+  
+    const randomX = Math.random() * (screenWidth - buttonWidth);
+    const randomY = Math.random() * (screenHeight - buttonHeight);
+  
+    button.style.left = randomX + "px";
+    button.style.top = randomY + "px";
+    setTimeout(pauseAudio, 4000);
+  }
+});
+
+function pauseAudio(){
+  myaudio.pause();
+}
+
+
+
+
+
+
+
